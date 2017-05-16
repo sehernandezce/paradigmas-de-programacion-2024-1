db3045a5ff45976c16c2e52ccb232086b5cbed1b
Santiago Carvajal <scarvajalc@unal.edu.co>
2017-05-15 20:40:37 -0500

    Menú  de contenido para facilitar navegar por las secciones, agregado (Boton para regresar al menú de contenido agregado)

---
+++ proglogica/tutoriales/prolog-gh-pages/js/prolog.js
@@ -0,0 +1,25 @@
+window.onscroll = function() {scrollFunction()};
+
+var mediaquery = window.matchMedia("(max-width: 64em)");
+if (mediaquery.matches) {
+	alert("sdfsdf");
+   var b = document.getElementById("myBtn").value = "Bebe";
+   alert(b);
+} else {
+  // mediaquery no es 600
+}
+
+function scrollFunction() {
+    if (document.body.scrollTop > 640 || document.documentElement.scrollTop > 640) {
+        document.getElementById("myBtn").style.display = "block";
+    } else {
+        document.getElementById("myBtn").style.display = "none";
+    }
+}
+
+
+function topFunction() {
+    document.body.scrollTop = 640; 
+    document.documentElement.scrollTop = 640; 
+}
+
